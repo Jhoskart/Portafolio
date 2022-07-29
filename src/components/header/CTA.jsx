4075a5141f65065cdcb939010ac48d36b845b8e0
Jhoskar Toro <jhoskartoro@gmail.com>
2022-07-28 21:10:11 -0400

    cv en ingles

--- src/components/header/CTA.jsx
+++ src/components/header/CTA.jsx
@@ -1,12 +1,18 @@
 import React from 'react'
 import CV from '../../assets/cv.pdf'
+import CVEN from '../../assets/cvEN.pdf'
 import { useTranslation } from 'react-i18next'
 
 const CTA = () => {
   const [t, i18n] = useTranslation("global")
+  const idioma = localStorage.getItem("idioma")
   return (
     <div className='cta'>
+      {idioma === "es" ? (
         <a href={CV} download className='btn'>{t("header.cv")}</a>
+      ):(
+        <a href={CVEN} download className='btn'>{t("header.cv")}</a>
+      )}
         <a href="#contact" className='btn btn-primary'>{t("header.accion")}</a>
     </div>
   )
